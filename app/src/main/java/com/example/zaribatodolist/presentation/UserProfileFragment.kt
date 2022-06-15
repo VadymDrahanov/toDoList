ca71a8e1769c81c3ff679b0d184bb3a5659be10f
Vadym Drahanov <v>
2022-06-15 11:18:43 +0300

    ViewModel args and view model factory sample

--- app/src/main/java/com/example/zaribatodolist/presentation/UserProfileFragment.kt
+++ app/src/main/java/com/example/zaribatodolist/presentation/UserProfileFragment.kt
@@ -14,28 +14,34 @@ class UserProfileFragment : Fragment() {
     private var _binding: FragmentUserProfileBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var vm: UserProfileViewModel
+    private lateinit var viewModel: UserProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         _binding = FragmentUserProfileBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        vm = ViewModelProvider(this).get(UserProfileViewModel::class.java)
+        val viewModelFactory = UserProfileViewModelFactory(10)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(UserProfileViewModel::class.java)
+        viewModel.apply {
+            setUserName()
 
-        vm.setUserName()
-        vm.setUserAge()
+            nameLive.observe(viewLifecycleOwner) { text ->
+                binding.userNameTV.text = text
+            }
 
-        vm.nameLive.observe(viewLifecycleOwner, { text->
-            binding.userNameTV.text = text
-        })
+            ageLive.observe(viewLifecycleOwner) { text->
+                binding.userAgeTV.text = text.toString()
+            }
+        }
 
-        vm.ageLive.observe(viewLifecycleOwner, { text->
-            binding.userAgeTV.text = text.toString()
-        })
+        binding.button.setOnClickListener {
+            viewModel.setUserAge()
+        }
 
         return view
     }
--- app/src/main/java/com/example/zaribatodolist/presentation/UserProfileViewModel.kt
+++ app/src/main/java/com/example/zaribatodolist/presentation/UserProfileViewModel.kt
@@ -2,18 +2,27 @@ package com.example.zaribatodolist.presentation
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
-class UserProfileViewModel : ViewModel() {
+class UserProfileViewModel(private val arg: Int) : ViewModel() {
 
     val nameLive = MutableLiveData<String>()
-    val ageLive = MutableLiveData<Int>()
+
+    val ageLive = MutableLiveData<Int>(0)
 
     fun setUserName() {
         nameLive.value = "John Doe"
     }
 
     fun setUserAge() {
-        ageLive.value = 32
+        ageLive.value?.let {
+            ageLive.value = it + arg
+        }
     }
+}
 
+class UserProfileViewModelFactory(val arg: Int) : ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(Int::class.java).newInstance(arg)
+    }
 }
